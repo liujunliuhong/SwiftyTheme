920e7efdc8313ce10501b23482d753d74ac16946
liujun <1035841713@qq.com>
2020-04-29 11:14:23 +0800

    first commit

---
+++ SwiftyTheme/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SwiftyTheme
+//
+//  Created by apple on 2020/4/29.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        
+        return true
+    }
+}
+
---
+++ SwiftyTheme/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SwiftyTheme
+//
+//  Created by apple on 2020/4/29.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+
